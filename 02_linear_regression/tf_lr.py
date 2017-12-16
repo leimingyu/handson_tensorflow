731ad44be1dbcf3dc3c477aa9ee2803bb2ae24c8
Leiming Yu <leimingyu830@gmail.com>
2017-12-16 07:16:10 -0500

    linear regression on housing data

---
+++ 02_linear_regression/tf_lr.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+
+housing = fetch_california_housing()
+m,n=housing.data.shape
+print("housing data : ({}, {})".format(m, n))
+
+housing_data_plus_bias = np.c_[np.ones((m,1)), housing.data]
+print housing_data_plus_bias.shape
+
+X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+XT = tf.transpose(X)
+theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)) , XT), y)
+
+with tf.Session() as sess:
+    theta_value = theta.eval()
+
+print theta_value
+
