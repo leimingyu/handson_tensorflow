c2b2abb820b078ae91c97068d9efe38293ef2f22
Leiming Yu <leimingyu830@gmail.com>
2017-12-16 10:35:21 +0000

    global initializer

--- 01_creating_grpah_and_run_session/main.py
+++ 01_creating_grpah_and_run_session/main.py
@@ -5,13 +5,41 @@ x = tf.Variable(3, name= "x")
 y = tf.Variable(4, name= "x")
 f = x * x + y * y
 
-sess = tf.Session()
 
+#
+# method 1
+#
+print "method 1"
+sess = tf.Session()
 sess.run(x.initializer)
 sess.run(y.initializer)
-
 result = sess.run(f)
+print result
+sess.close()
 
+
+
+
+#
+# method 2
+#
+print "method 2"
+with tf.Session() as sess:
+	x.initializer.run()
+	y.initializer.run()
+	result = f.eval()
 print result
 
-sess.close()
+
+
+#
+# method 3
+#
+print "method 3"
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+	init.run() # init all the variables
+	result = f.eval()
+print result
